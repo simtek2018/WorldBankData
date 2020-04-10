ec8a441b51c175a5536834bcc230f11f609ceeef
Peter Baker <pcbaker@simtekgamedevelopment.com>
2020-04-10 16:30:14 -0400

    Added line chart

--- eclipse-workspace2/Rithwik_Method2/src/population/Charts.java
+++ eclipse-workspace2/Rithwik_Method2/src/population/Charts.java
@@ -8,16 +8,18 @@ import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class Charts extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	public Charts(ArrayList<WorldBankEntry> data) {
-		PieDataset dataset = createDataset(data);
-		JFreeChart ch = ChartFactory.createPieChart3D("Populate", dataset);
+	public Charts(WorldBankEntry data) {
+		XYSeriesCollection dataset = createDataset(data);
+		JFreeChart ch = ChartFactory.createXYLineChart(data.getCountry(), "Years", "Population", 
+				dataset, PlotOrientation.VERTICAL, true, true, false);
 		ChartPanel chartPanel = new ChartPanel(ch);
 		ch.removeLegend();
 		chartPanel.setPreferredSize(new Dimension(500, 300));
@@ -25,16 +27,19 @@ public class Charts extends JFrame {
 		setContentPane(chartPanel);
 		setVisible(true);
 		pack();
-		
 	}
 
-	private PieDataset createDataset(ArrayList<WorldBankEntry> data) {
-		DefaultPieDataset chartData = new DefaultPieDataset();
-		
-		for (WorldBankEntry k : data) {
-			
-			chartData.setValue(k.getCountry(), k.getTotPopulation());
+	private XYSeriesCollection createDataset(WorldBankEntry data) {
+		XYSeriesCollection  chartData = new XYSeriesCollection();	
+		XYSeries series = new XYSeries("Population");
+		ArrayList<Long> popLst = data.getPopulation();
+		int i=0;
+		for (Long k : popLst) {
+			int year = 1960 + i;
+			i++;
+			series.add(year, k);
 		}
+		chartData.addSeries(series);
 		return chartData;
 	}
 }
